c0ae87277f206a2d3ba8ecf340ed3416057f90ba
vladverheij <vladverheij@gmail.com>
2023-11-23 10:59:11 +0100

    add pencil constructor and extend class function

--- m10prog-les_2/public/index.php
+++ m10prog-les_2/public/index.php
@@ -2,14 +2,53 @@
 
 class Pencil
 {
-    private $wood;
-    private $graphite;
-    private $clay;
-    private $rubber;
-    private $sharpness;
+    public string $color;
+    public float $length;
+    public float $sharpness;
 
-    function sharpen(): string
+    function __construct($color = '', $sharpness = 0)
     {
-        return $this->sharpness;
+        $this->length = 20;
+        $this->sharpness = $sharpness;
+        $this->color = $color;
+    }
+
+    function sharpen($sharpnessIncrease)
+    {
+        for ($i = 0; $i < $sharpnessIncrease; $i++) {
+            $this->length -= 0.2;
+            $this->sharpness++;
+        }
+
+        if ($this->length <= 0) {
+            $this->length = 0;
+            $this->sharpness = 0;
+        }
+
+        if ($this->color != null) {
+            echo "Color: {$this->color} <br> Sharpness: {$this->sharpness}% <br> Length: " . round($this->length, 1) . "cm";
+        } else {
+            echo "Sharpness: {$this->sharpness}% <br> Length: " . round($this->length, 1) . "cm";
+        }
     }
 }
+
+class BicPencil extends Pencil
+{
+    function __construct($color = '', $sharpness = 0)
+    {
+        $this->length = 20;
+        $this->sharpness = $sharpness;
+        $this->color = $color;
+    }
+}
+
+echo "Pencil: <br>";
+$pencil = new Pencil();
+echo $pencil->sharpen(98);
+
+echo "<br> <br>";
+
+echo "Bic Pencil: <br>";
+$bicpencil = new BicPencil("Gray");
+echo $bicpencil->sharpen(2);
